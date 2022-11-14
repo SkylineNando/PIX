ac4f29c45ddf1d1e48a446ddd8d7e54c012f0fce
Fernando Bueno <buenofernando13@gmail.com>
2022-11-14 11:35:54 -0300

    Create config.php

---
+++ PIX-API-MERCADO-PAGO-V1/config.php
@@ -0,0 +1,9 @@
+<?php
+// Conexao com banco de dados
+$servername     = "localhost";
+$username       = "root";
+$password       = "";
+$db_name        = "banco";
+
+$conexao = mysqli_connect($servername, $username, $password, $db_name);
+?>
