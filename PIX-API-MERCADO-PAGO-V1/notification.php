77435dcf511ab82de2c2f56943410397823c3b98
Fernando Bueno <buenofernando13@gmail.com>
2022-11-14 11:33:22 -0300

    Create notification.php

---
+++ PIX-API-MERCADO-PAGO-V1/notification.php
@@ -0,0 +1,26 @@
+<?php include('config.php');?>
+<?php
+
+$collector_id = 51503947553;
+
+$curl = curl_init();
+
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://api.mercadopago.com/v1/payments/'.$collector_id,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+    CURLOPT_HTTPHEADER => array(
+        'accept: application/json',
+        'content-type: application/json',
+        'Authorization: Bearer {Access Token}'
+    ),
+    ));
+    $response = curl_exec($curl);
+    $resultado = json_decode($response);
+    
+curl_close($curl);
+
+$update = "UPDATE status SET
+    status		= '".$resultado->status."'
+WHERE codigo='$collector_id'";
+mysqli_query($conexao, $update);
